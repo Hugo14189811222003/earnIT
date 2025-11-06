adb4a143f11406461ac09e1f75daafaa47186168
Hugo Arcos <hugo.arcos@paramq.com>
2025-11-06 13:55:52 -0600

    cambio a varchar de duracion

--- cursos/database/db.sql
+++ cursos/database/db.sql
@@ -5,7 +5,7 @@ create table curso (
 id_curso int auto_increment primary key,
 titulo varchar(100) not null,
 descripcion text,
-duracion int,
+duracion varchar(50),
 progreso varchar(50),
 fecha_creacion timestamp default current_timestamp,
 fecha_actualizacion timestamp default current_timestamp on update current_timestamp
